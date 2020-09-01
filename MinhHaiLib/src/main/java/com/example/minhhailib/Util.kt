c4ac89f22eb4fbfc4b342dd33d8484f5e55eab4d
Hải minh <hailmdev169@gmail.com>
2020-09-01 13:41:59 +0700

    upudate

--- MinhHaiLib/src/main/java/com/example/minhhailib/Util.kt
+++ MinhHaiLib/src/main/java/com/example/minhhailib/Util.kt
@@ -1,10 +1,16 @@
 package com.example.minhhailib
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+
 
 fun AppCompatActivity.showToast(message: String? = null) {
     message?.run {
         Toast.makeText(this@showToast, this, Toast.LENGTH_SHORT).show()
     }
+}
+
+fun showMessage(c: Context?, message: String?) {
+    Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
 }
