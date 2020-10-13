716564db91022ff3698585b6c6e4aea90cc841c2
Hải minh <hailmdev169@gmail.com>
2020-10-13 15:52:48 +0700

    update

--- app/src/main/java/com/lib/example/MainActivity.kt
+++ app/src/main/java/com/lib/example/MainActivity.kt
@@ -2,15 +2,13 @@ package com.lib.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lib.marketplace.app.ApplicationIpos
-import com.lib.marketplace.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ApplicationIpos.instance = ApplicationIpos()
-        ApplicationIpos.instance.loadData(companyId = "aaaa")
-        Log.e("aaa","bb: "+ ApplicationIpos.instance.cartBussiness?.companyId)
+//        ApplicationIpos.instance = ApplicationIpos()
+//        ApplicationIpos.instance.loadData(companyId = "aaaa")
+//        Log.e("aaa","bb: "+ ApplicationIpos.instance.cartBussiness?.companyId)
     }
 }
